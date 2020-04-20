b690c00929b03a249c291c29480905759c76f6b3
Sebastian Romero <sfromero23@gmail.com>
2020-04-19 21:45:15 -0300

    Version final con los 11 ejercicios.

--- SolutionLPPAHtml/EjerciciosHtml.WebSite/Controllers/HomeController.cs
+++ SolutionLPPAHtml/EjerciciosHtml.WebSite/Controllers/HomeController.cs
@@ -41,5 +41,25 @@ namespace EjerciciosHtml.WebSite.Controllers
         {
             return View();
         }
+        public ActionResult Ejercicio7()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio8()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio9()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio10()
+        {
+            return View();
+        }
+        public ActionResult Ejercicio11()
+        {
+            return View();
+        }
     }
 }
